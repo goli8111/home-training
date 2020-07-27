1a9328e40697e0a783ce0cbe0e62b81ac5bf2e9c
goli8111 <60034176+goli8111@users.noreply.github.com>
2020-07-28 01:34:27 +0900

    posenet 업데이트 코드 제거

---
+++ precache-manifest.cafedf1c3fbd6287204a86f83550e4be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de68e69abbe8f01765f246e6f59aaa46",
+    "url": "/home-training/index.html"
+  },
+  {
+    "revision": "095ece431644aed57ce9",
+    "url": "/home-training/static/css/2.f524894f.chunk.css"
+  },
+  {
+    "revision": "d6cd9eebb838c704d382",
+    "url": "/home-training/static/css/main.73315ff7.chunk.css"
+  },
+  {
+    "revision": "095ece431644aed57ce9",
+    "url": "/home-training/static/js/2.2baa03ff.chunk.js"
+  },
+  {
+    "revision": "e46609aec777f9260d21834116f9e6de",
+    "url": "/home-training/static/js/2.2baa03ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6cd9eebb838c704d382",
+    "url": "/home-training/static/js/main.c3cb3e8e.chunk.js"
+  },
+  {
+    "revision": "876ac28cad8de66e04f3",
+    "url": "/home-training/static/js/runtime-main.0c455910.js"
+  }
+]);
