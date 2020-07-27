0a39b76cb2a948324b3c4232fbd3ac5012b61997
goli8111 <60034176+goli8111@users.noreply.github.com>
2020-07-28 02:36:01 +0900

    포즈넷 초기화와 업데이트 코드 추가

---
+++ precache-manifest.9ced9b55b8cacf6f520121165794bc20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cff2e88681932e59e2618bfd14c11f3f",
+    "url": "/home-training/index.html"
+  },
+  {
+    "revision": "095ece431644aed57ce9",
+    "url": "/home-training/static/css/2.f524894f.chunk.css"
+  },
+  {
+    "revision": "faf02c8b9c35ad1111c6",
+    "url": "/home-training/static/css/main.73315ff7.chunk.css"
+  },
+  {
+    "revision": "095ece431644aed57ce9",
+    "url": "/home-training/static/js/2.2baa03ff.chunk.js"
+  },
+  {
+    "revision": "e46609aec777f9260d21834116f9e6de",
+    "url": "/home-training/static/js/2.2baa03ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faf02c8b9c35ad1111c6",
+    "url": "/home-training/static/js/main.8b472f52.chunk.js"
+  },
+  {
+    "revision": "876ac28cad8de66e04f3",
+    "url": "/home-training/static/js/runtime-main.0c455910.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home-training/precache-manifest.e107aa2de8cd60ca2ea2ef3a64082f43.js"
+  "/home-training/precache-manifest.9ced9b55b8cacf6f520121165794bc20.js"
 );
 
 self.addEventListener('message', (event) => {
